57d8c6cab25cb65a770a5338ca38f180c0372b61
ilya krylov <pptestkrylov@gmail.com>
2026-02-14 13:33:58 +0300

    Правки дз

--- src/test/java/form/PracticeFormTest.java
+++ src/test/java/form/PracticeFormTest.java
@@ -4,52 +4,52 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
 
-    File downloadFile = new File("src/test/resources/sample-clouds-400x300.jpg");
-
     @BeforeAll
     static void beforeAll() {
-        baseUrl = "https://demoqa.com/automation-practice-form";
+        baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1980x1080";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void correctRegistration() {
-        open(baseUrl);
+        open(baseUrl+"/automation-practice-form");
         $("#firstName").setValue("ivan");
         $("#lastName").setValue("ivanov");
         $("#userEmail").setValue("ivanov@gmail.com");
-        executeJavaScript("arguments[0].click();", $("label[for='gender-radio-1']"));
         $("#userNumber").setValue("89990007766");
+        $("#genterWrapper").$(byText("Female")).click();
         $("#dateOfBirthInput").click();
-        $("#dateOfBirthInput").setValue("10 Feb 2026");
-        $("#subjectsInput").sendKeys("a");
-        $("#react-select-2-option-2").click();
-        executeJavaScript("arguments[0].click();", $("label[for='hobbies-checkbox-1']"));
-        $("#uploadPicture").uploadFile(downloadFile);
+        $(".react-datepicker__year-select").click();
+        $("[value='2025']").click();
+        $(".react-datepicker__month-select").click();
+        $("[value='4']").click();
+        $("[aria-label='Choose Sunday, May 11th, 2025']").click();
+        $("#subjectsInput").setValue("arts").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("sample-clouds-400x300.jpg");
         $("#currentAddress").setValue("my_current_address");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
         $("#submit").click();
-
         //Блок проверки результата
-        $x("//tbody/tr[1]/td[2]").shouldHave(text("ivan ivanov"));
-        $x("//tbody/tr[2]/td[2]").shouldHave(text("ivanov@gmail.com"));
-        $x("//tbody/tr[3]/td[2]").shouldHave(text("Male"));
-        $x("//tbody/tr[4]/td[2]").shouldHave(text("8999000776"));
-        $x("//tbody/tr[5]/td[2]").shouldHave(text("10 February,202610"));
-        $x("//tbody/tr[6]/td[2]").shouldHave(text("Arts"));
-        $x("//tbody/tr[7]/td[2]").shouldHave(text("Sport"));
-        $x("//tbody/tr[8]/td[2]").shouldHave(text("sample-clouds-400x300.jpg"));
-        $x("//tbody/tr[9]/td[2]").shouldHave(text("my_current_address"));
-        $x("//tbody/tr[10]/td[2]").shouldHave(text("NCR Delhi"));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("ivan ivanov"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("ivanov@gmail.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("8999000776"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("11 May,2025"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Arts"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sport"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("sample-clouds-400x300.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("my_current_address"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
     }
 }
