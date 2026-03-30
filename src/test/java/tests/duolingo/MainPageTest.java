1d67cf60d3cfe950a6df0d39028db6617a338ffe
Krylov Ilya <pptestkrylov@gmail.com>
2026-03-30 13:55:45 +0300

    ДЗ Добавил правки в дз

--- src/test/java/tests/duolingo/MainPageTest.java
+++ src/test/java/tests/duolingo/MainPageTest.java
@@ -1,6 +1,5 @@
 package tests.duolingo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -9,24 +8,14 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import tests.demoqa.testdata.TestData;
 import tests.duolingo.pages.LearnLanguages;
-import tests.duolingo.pages.LoginPage;
 import tests.duolingo.pages.MainPage;
 
 import java.util.stream.Stream;
 
-import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainPageTest extends TestBase {
-
-    @BeforeEach
-    public void openPage() {
-        open(baseUrl);
-    }
-
-    MainPage mainPage = new MainPage();
 
     private static Stream<Arguments> checkErrorMessageIfLogopassIncorrect() {
         TestData t = new TestData();
@@ -44,6 +33,7 @@ public class MainPageTest extends TestBase {
             "Deutsch; Effektiv und kostenlos eine Sprache lernen – und dabei Spaß haben!",
     }, delimiter = ';')
     public void greetengsElementShouldChangeLanguageThenLanguageIsChanges(String l, String result) {
+        MainPage mainPage = new MainPage();
         mainPage.changeSiteLanguage(l);
         assertEquals(result, mainPage.getTextFromGreetingsTitle());
     }
@@ -52,6 +42,7 @@ public class MainPageTest extends TestBase {
     @DisplayName("Проверка выбора изучаемого языка")
     @EnumSource(LearnLanguages.class)
     void checkMessageThenChooseLearnLanguage(LearnLanguages l) {
+        MainPage mainPage = new MainPage();
         mainPage.changeSiteLanguage("русский")
                 .acceptCookie()
                 .changeLearnLanguage(l.getLanguage());
@@ -63,6 +54,7 @@ public class MainPageTest extends TestBase {
     @DisplayName("Проверка логина с некорреткными логином и паролем")
     @MethodSource("checkErrorMessageIfLogopassIncorrect")
     void checkErrorMessageIfLogopassIncorrect(String email, String password) {
+        MainPage mainPage = new MainPage();
         String result = mainPage.changeSiteLanguage("русский")
                 .acceptCookie()
                 .goToLoginPage()
--- src/test/java/tests/duolingo/TestBase.java
+++ src/test/java/tests/duolingo/TestBase.java
@@ -3,8 +3,11 @@ package tests.duolingo;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
     @BeforeAll
@@ -13,6 +16,12 @@ public class TestBase {
         Configuration.baseUrl = "https://ko.duolingo.com/";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
+    }
+
+
+    @BeforeEach
+    public void openPage() {
+        open(baseUrl);
     }
 
     @AfterEach
