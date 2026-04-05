d74d0f9bb9b08664d1f6b3417a6d8dec42f31cd9
Krylov Ilya <pptestkrylov@gmail.com>
2026-04-05 12:13:14 +0300

    Исправления дз 8

--- src/test/java/tests/filestest/FilesTest.java
+++ src/test/java/tests/filestest/FilesTest.java
@@ -22,27 +22,51 @@ public class FilesTest {
     ClassLoader cl = FilesTest.class.getClassLoader();
 
     @Test
-    @DisplayName("Проверка вложенный файлов в zip архиве")
-    void testFilesInZip() throws IOException, CsvException {
+    @DisplayName("Чтение и проверка PDF файла из архива")
+    void readAndCheckDataFromPdfFileInZipArchive() throws IOException {
         try (ZipInputStream zis = new ZipInputStream(
                 cl.getResourceAsStream("files1.zip")
         )) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
-                assertTrue(entry.getSize() != 0);
-                String fileName = entry.getName();
-                if (fileName.endsWith(".pdf")) {
+                if (entry.getName().endsWith(".pdf")) {
+                    assertTrue(entry.getSize() != 0);
                     PDF pdf = new PDF(zis);
                     String resultData = pdf.text;
                     assertTrue(resultData.contains("ДЕПАРТАМЕНТ ПРИРОДОПОЛЬЗОВАНИЯ И ОХРАНЫ ОКРУЖАЮЩЕЙ СРЕДЫ ГОРОДА МОСКВЫ"));
                 }
-                if (fileName.endsWith(".csv")) {
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Чтение и проверка CSV файла из архива")
+    void readAndCheckDataFromCsvFileInZipArchive() throws IOException, CsvException {
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("files1.zip")
+        )) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().endsWith(".csv")) {
+                    assertTrue(entry.getSize() != 0);
                     CSVReader csvReader = new CSVReader(new InputStreamReader(zis));
                     List<String[]> strings = csvReader.readAll();
                     assertEquals(38, strings.size());
                     assertArrayEquals(new String[]{"name", "phoneNumber", "email", "address", "userAgent", "hexcolor"}, strings.get(0));
+                    break;
                 }
-                if (fileName.endsWith(".xlsx")) {
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Чтение и проверка .xlsx файла из архива")
+    void readAndCheckDataFromXslxFileInZipArchive() throws IOException {
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("files1.zip"))) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().endsWith(".xlsx")) {
                     XLS xls = new XLS(zis);
                     String res = xls.excel.getSheetAt(0).getRow(4).getCell(0).getStringCellValue();
                     assertEquals("00127/26", res);
@@ -50,6 +74,7 @@ public class FilesTest {
             }
         }
     }
+
 
     @Test
     @DisplayName("Чтение и проверка файлов из json файла")
