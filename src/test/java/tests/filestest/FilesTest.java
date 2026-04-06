993ee800bdb098447a6a523404684dd589f2b9a3
Krylov Ilya <pptestkrylov@gmail.com>
2026-04-06 11:49:00 +0300

    Исправления дз 8, добавил пустую папку дя проверки негативного теста

--- src/test/java/tests/filestest/FilesTest.java
+++ src/test/java/tests/filestest/FilesTest.java
@@ -24,55 +24,61 @@ public class FilesTest {
     @Test
     @DisplayName("Чтение и проверка PDF файла из архива")
     void readAndCheckDataFromPdfFileInZipArchive() throws IOException {
-        try (ZipInputStream zis = new ZipInputStream(
-                cl.getResourceAsStream("filesEmpty.zip")
-        )) {
-            ZipEntry entry;
-
-            while ((entry = zis.getNextEntry()) != null) {
-                assertTrue(entry.getSize()>0);
-                if (entry.getName().endsWith(".pdf")) {
-                    PDF pdf = new PDF(zis);
-                    String resultData = pdf.text;
-                    assertTrue(resultData.contains("ДЕПАРТАМЕНТ ПРИРОДОПОЛЬЗОВАНИЯ И ОХРАНЫ ОКРУЖАЮЩЕЙ СРЕДЫ ГОРОДА МОСКВЫ"));
-                }
-            }
-        }
-    }
-
-    @Test
-    @DisplayName("Чтение и проверка CSV файла из архива")
-    void readAndCheckDataFromCsvFileInZipArchive() throws IOException, CsvException {
+        boolean fileFound = false;
         try (ZipInputStream zis = new ZipInputStream(
                 cl.getResourceAsStream("files1.zip")
         )) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
-                assertTrue(entry.getSize()>0);
+                if (entry.getName().endsWith(".pdf")) {
+                    fileFound = true;
+                    PDF pdf = new PDF(zis);
+                    String resultData = pdf.text;
+                    assertTrue(resultData.contains(
+                            "ДЕПАРТАМЕНТ ПРИРОДОПОЛЬЗОВАНИЯ И ОХРАНЫ ОКРУЖАЮЩЕЙ СРЕДЫ ГОРОДА МОСКВЫ"));
+                }
+            }
+        }
+        assertTrue(fileFound, "Файл с расширением .pdf не найден");
+    }
+
+    @Test
+    @DisplayName("Чтение и проверка CSV файла из архива")
+    void readAndCheckDataFromCsvFileInZipArchive() throws IOException, CsvException {
+        boolean fileFound = false;
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("file1.zip")
+        )) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith(".csv")) {
+                    fileFound = true;
                     CSVReader csvReader = new CSVReader(new InputStreamReader(zis));
                     List<String[]> strings = csvReader.readAll();
                     assertEquals(38, strings.size());
                     assertArrayEquals(new String[]{"name", "phoneNumber", "email", "address", "userAgent", "hexcolor"}, strings.get(0));
                 }
             }
+            assertTrue(fileFound, "Файл с расширением .csv не найден");
         }
     }
 
     @Test
     @DisplayName("Чтение и проверка .xlsx файла из архива")
     void readAndCheckDataFromXslxFileInZipArchive() throws IOException {
+        boolean fileFound = false;
         try (ZipInputStream zis = new ZipInputStream(
-                cl.getResourceAsStream("files1.zip"))) {
+                cl.getResourceAsStream("fileCsv.zip"))) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
-                assertTrue(entry.getSize()>0);
                 if (entry.getName().endsWith(".xlsx")) {
+                    fileFound = true;
                     XLS xls = new XLS(zis);
                     String res = xls.excel.getSheetAt(0).getRow(4).getCell(0).getStringCellValue();
                     assertEquals("00127/26", res);
                 }
             }
+            assertTrue(fileFound, "Файл с расширением .xlsx не найден");
         }
     }
 
