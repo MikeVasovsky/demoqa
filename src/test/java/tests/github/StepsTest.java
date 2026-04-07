bdb8234200299a98f1ad2f01df76b9ef5c140ed9
Krylov Ilya <pptestkrylov@gmail.com>
2026-04-07 17:08:05 +0300

    ДЗ 9 Добавил тест с логгированием через step

---
+++ src/test/java/tests/github/StepsTest.java
@@ -0,0 +1,38 @@
+package tests.github;
+
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import tests.github.pages.RepositoriesSearchPage;
+import tests.github.pages.RepositoryPage;
+import tests.github.pages.StartPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
+public class StepsTest extends TestBase {
+    StartPage sp = new StartPage();
+    RepositoriesSearchPage rsp = new RepositoriesSearchPage();
+    RepositoryPage rp = new RepositoryPage();
+
+    @Test
+    @DisplayName("Проверка issue по тексту с шагами через лямбды")
+    void checkIssueNameByText() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        step("Найти репозиторий", () -> {
+            sp.searchRepo("MikeVasovsky/demoqa");
+        });
+        step("Выбрать репозиторий", () -> {
+            rsp.chooseRepository("MikeVasovsky/demoqa");
+        });
+        step("Кликнуть на раздел issue", () -> {
+            rp.clickIssueTab();
+        });
+        step("Найти issue и проверить его название", () -> {
+            rp.findIssueByName("issue for allure homework");
+
+        });
+    }
+}
--- src/test/java/tests/github/TestBase.java
+++ src/test/java/tests/github/TestBase.java
@@ -1,6 +1,8 @@
 package tests.github;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,8 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 public class TestBase {
+
     @BeforeAll
     static void setUp() {
         Configuration.browserSize = "1980x1080";
@@ -21,7 +25,10 @@ public class TestBase {
 
     @BeforeEach
     public void openPage() {
-        open(baseUrl);
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        step("Открываем страницу github", ()->{
+            open(baseUrl);
+        });
     }
 
     @AfterEach
