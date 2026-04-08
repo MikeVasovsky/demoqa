e63f60d739af3b53f95f277a982be80c9e38e925
Krylov Ilya <pptestkrylov@gmail.com>
2026-04-08 16:48:34 +0300

    Добавил простой тест для jenkins

---
+++ src/test/java/tests/jenkins/SimpleTest.java
@@ -0,0 +1,13 @@
+package tests.jenkins;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    void test1(){
+        assertTrue(true);
+    }
+}
