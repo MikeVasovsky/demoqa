b81be30f8c71f7544ff833398e0ea6667b2f43f5
Krylov Ilya <pptestkrylov@gmail.com>
2026-04-13 12:31:13 +0300

    REST-HW Добавил второй тест тест

---
+++ src/test/java/tests/rest/BaseTest.java
@@ -0,0 +1,12 @@
+package tests.rest;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    public static void beforeAll(){
+        RestAssured.baseURI = "https://selenoid.autotests.cloud";
+    }
+}
--- src/test/java/tests/rest/StatusTest.java
+++ src/test/java/tests/rest/StatusTest.java
@@ -3,21 +3,22 @@ package tests.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.is;
 
-public class StatusTest {
+public class StatusTest extends BaseTest{
 
     @Test
     void getTotalTest() {
-        given()
-                .when()
-                .get("https://selenoid.autotests.cloud/status")
+        given().log().all()
+                .get("/status")
                 .then()
+                .log().all()
                 .statusCode(200)
+                .body(matchesJsonSchemaInClasspath(
+                        "shemas/json_selenoid_status_shema.json")
+                )
                 .body("total", is(5));
     }
-
-
-
 
 }
